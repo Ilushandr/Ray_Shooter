dcd6727c80053006da20a7e2f28e45ddce1fc7e0
ilushandr <ilushandr@gmail.com>
2020-12-22 20:39:08 +0500

    commit with raycast vision update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from math import cos, sin, atan2
+from math import cos, sin, atan2, pi
 
 
 class Wall(pygame.sprite.Sprite):
@@ -9,17 +9,39 @@ class Wall(pygame.sprite.Sprite):
         self.y = y
         self.w = w
         self.h = h
+        self.image = pygame.Surface((w, h),
+                                    pygame.SRCALPHA, 32)
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = pygame.Rect(x, y, w, h)
 
     def update(self):
         pass
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, radius, fov):
+class Gun:
+    def __init__(self):
+        pass
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius=10):
         super().__init__(all_sprites)
-        self.x = width // 2
-        self.y = height // 2
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = pygame.Rect(self.x, self.y, 2 * radius, 2 * radius)
+        pygame.draw.circle(self.image, 'red',
+                           (radius, radius), radius)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, fov, radius=10):
+        super().__init__(all_sprites)
+        self.x = x
+        self.y = y
         self.radius = radius
         self.fov = fov  # Угол обзора игрока
         self.image = pygame.Surface((2 * radius, 2 * radius),
@@ -27,8 +49,9 @@ class Player(pygame.sprite.Sprite):
         pygame.draw.circle(self.image, 'white',
                            (radius, radius), radius)
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = pygame.Rect(self.x, self.y, 2 * radius, 2 * radius)
+        self.rect = pygame.Rect(self.x, self.y, 20, 20)
         self.obstacles = [wall.rect for wall in walls]  # Спиоск всех преград
+        self.enemies = [enemy for enemy in enemies]
 
     def ray_cast(self):
         mx, my = pygame.mouse.get_pos()
@@ -36,7 +59,7 @@ class Player(pygame.sprite.Sprite):
         aim_x, aim_y = x, y
         view_angle = atan2(my - y, mx - x)  # Считает угол относительно курсора
         # Начальные координаты многоугольника, по которому рисуется рейкаст
-        coords = [(x, y)]
+        coords = self.start_ray_coords(x, y, view_angle)
         for a in range(-self.fov, self.fov + 1):  # Цикл по углу обзора
             # Заранее считаем синус и косинус, шоб ресурсы потом не тратить
             # На 100 делится для точности и птушо в for пихается тока целые числа,
@@ -48,12 +71,13 @@ class Player(pygame.sprite.Sprite):
 
             # Цикл увеличения дистанции. Чем больше шаг, тем выше произ-ть,
             # но ниже точность рейкаста
-            for c in range(0, 1000, 20):
+            step = 15
+            for c in range(0, 1000, step):
                 point.x = x + c * cos_a
                 point.y = y + c * sin_a
                 if point.collidelistall(self.obstacles):  # Если точка достигает стены
                     # Тут уже начинается подгон точки под границы ректа бинарным поиском
-                    l, r = c - 50, c
+                    l, r = c - step, c
                     while r - l > 1:
                         m = (r + l) / 2
                         point.x = x + m * cos_a
@@ -62,37 +86,53 @@ class Player(pygame.sprite.Sprite):
                             r = m
                         else:
                             l = m
+
                     break
             if a == 0:
                 aim_x, aim_y = point.x, point.y
             coords.append((point.x, point.y))
-
-        #  Наконец рисуем полигон
-        pygame.draw.polygon(screen, pygame.Color(255, 255, 255), coords)
+        # Наконец рисуем полигон
+        pygame.draw.polygon(screen, 'black', coords)
         pygame.draw.line(screen, 'red', (x, y),
                          (aim_x, aim_y))
+
+    def start_ray_coords(self, x, y, a):
+        if -pi <= a <= -pi / 2:
+            return [(x, y), (width, height), (0, height), (0, 0),
+                    (width, 0), (width, height), (x, y)]
+        elif -pi / 2 <= a <= 0:
+            return [(x, y), (0, height), (0, 0), (width, 0),
+                    (width, height), (0, height), (x, y)]
+        elif 0 <= a <= pi / 2:
+            return [(x, y), (0, 0), (width, 0), (width, height),
+                    (0, height), (0, 0), (x, y)]
+        else:
+            return [(x, y), (width, 0), (width, height),
+                    (0, height), (0, 0), (width, 0), (x, y)]
 
     def movement(self, dx, dy):
         # Метод обрабатывает столкновение игрока с препятствиями и меняет его координаты
         # Изменение по x
-        self.rect.x += dx
-        for block in self.obstacles:
-            if self.rect.colliderect(block):
-                if dx < 0:
-                    self.rect.left = block.right
-                elif dx > 0:
-                    self.rect.right = block.left
-                break
+        if dx:
+            self.rect.x += dx
+            for block in self.obstacles:
+                if self.rect.colliderect(block):
+                    if dx < 0:
+                        self.rect.left = block.right
+                    elif dx > 0:
+                        self.rect.right = block.left
+                    break
 
         # Изменение по y
-        self.rect.y += dy
-        for block in self.obstacles:
-            if self.rect.colliderect(block):
-                if dy < 0:
-                    self.rect.top = block.bottom
-                elif dy > 0:
-                    self.rect.bottom = block.top
-                break
+        if dy:
+            self.rect.y += dy
+            for block in self.obstacles:
+                if self.rect.colliderect(block):
+                    if dy < 0:
+                        self.rect.top = block.bottom
+                    elif dy > 0:
+                        self.rect.bottom = block.top
+                    break
 
     def move_character(self):
         # Здесь происходит управление игроком
@@ -144,14 +184,16 @@ if __name__ == '__main__':
     display_info = pygame.display.Info()
     #  Достаются значения разрешения экрана из display_info
     size = width, height = display_info.current_w, display_info.current_h
-    screen = pygame.display.set_mode(size)
+    flags = pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF
+    screen = pygame.display.set_mode(size, flags=flags)
 
     all_sprites = pygame.sprite.Group()
     walls = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
 
     map = Map()
-    player = Player(10, 90)
-
+    Enemy(width // 4, height // 2, 20)
+    player = Player(width // 2, height // 2, 90)
     v = 5
     fps = 60
     clock = pygame.time.Clock()
@@ -161,9 +203,11 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill('black')
+        screen.fill('white')
+
         all_sprites.draw(screen)
         player.update()
+
         pygame.display.flip()
         clock.tick(fps)
         print(clock.get_fps())
